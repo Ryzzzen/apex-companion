fb336b4f849893ee029d1d81ed6fb6f5400a8f39
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-03-04 09:34:31 +0100

    Initial commit

---
+++ class/Module.js
@@ -0,0 +1,27 @@
+class Module {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.storage = new (require('node-storage'))(require('path').join(__dirname, '../data/' + id + '.json'));
+  }
+
+  async start() {
+    console.log(`[${this.name}] - Starting`);
+    this.started = true;
+  }
+
+  async stop() {
+    console.log(`[${this.name}] - Shutting down`);
+    this.started = false;
+  }
+
+  getDependencies() {
+    return [];
+  }
+
+  generateErrorMessage() {
+
+  }
+}
+
+module.exports = Module;
---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+require('moment').locale(process.env.LOCALE);
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const ApexCompanion = new (require('./class/ApexCompanion'))(client);
+
+ApexCompanion.on('shutdown', async function() {
+  console.log('Shutting down core modules.');
+
+  await ApexCompanion.shutdown();
+  await ApexCompanion.client.destroy();
+  process.timeout(process.exit, 5000);
+});
+
+ApexCompanion.load().then(x => {
+  client.on('ready', async () => {
+      await ApexCompanion.onReady(client);
+
+      client.on('message', msg => ApexCompanion.onMessage(msg));
+      console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of the guild.`);
+  });
+  client.login(process.env.DISCORD_TOKEN);
+});
